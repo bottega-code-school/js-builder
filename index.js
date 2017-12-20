bad182acb7b6d0927381155fc62b43235a3c3b97
Jordan Hudgens <jordan@wow.com>
2017-12-19 17:26:26 -0700

    Initial implementation with react template

---
+++ index.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const CHOICES = fs.readdirSync(`${__dirname}/templates`);
+
+const QUESTIONS = [
+  {
+    name: 'project-choice',
+    type: 'list',
+    message: 'What project template would you like to generate?',
+    choices: CHOICES
+  },
+  {
+    name: 'project-name',
+    type: 'input',
+    message: 'Project name:',
+    validate: function (input) {
+      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+      else return 'Project name may only include letters, numbers, underscores and hashes.';
+    }
+  },
+//  {
+//    name: 'project-path',
+//    type: 'input',
+//    message: 'Project path:',
+//  }
+];
+
+const CURR_DIR = process.cwd();
+
+inquirer.prompt(QUESTIONS)
+  .then(answers => {
+    const projectChoice = answers['project-choice'];
+    const projectName = answers['project-name'];
+    const templatePath = `${__dirname}/templates/${projectChoice}`;
+
+    fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+
+    createDirectoryContents(templatePath, projectName);
+  });
+
+function createDirectoryContents (templatePath, newProjectPath) {
+  const filesToCreate = fs.readdirSync(templatePath);
+
+  filesToCreate.forEach(file => {
+    const origFilePath = `${templatePath}/${file}`;
+
+    // get stats about the current file
+    const stats = fs.statSync(origFilePath);
+
+    if (stats.isFile()) {
+      const contents = fs.readFileSync(origFilePath, 'utf8');
+
+      const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
+      fs.writeFileSync(writePath, contents, 'utf8');
+    } else if (stats.isDirectory()) {
+      fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
+
+      // recursive call
+      createDirectoryContents(`${templatePath}/${file}`, `${newProjectPath}/${file}`);
+    }
+  });
+
+  if (stats.isFile()) {
+    const contents = fs.readFileSync(origFilePath, 'utf8');
+
+    if (file === '.npmignore') file = '.gitignore';
+
+    const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
+    fs.writeFileSync(writePath, contents, 'utf8');
+  }
+}
+
---
+++ templates/react-project/README.md
@@ -0,0 +1,13 @@
+# React/Redux Starter Project
+
+> Provided for the students of the [Bottega Code School](https://bottega.tech/)
+
+
+## Instructions
+
+- [ ] Run this command to clone this repo: `git@github.com:bottega-code-school/devcamp-react-starter.git`
+- [ ] Change into the directory in your terminal
+- [ ] Delete the `.git` directory (this will allow you to use your own git repository)
+- [ ] Run `git init`
+- [ ] Run `sudo npm install`
+- [ ] Run `npm start` and navigate to `localhost:8080` in your browser
