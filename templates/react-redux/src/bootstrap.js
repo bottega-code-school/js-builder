5b125463499a1aaed9e352b4a1b151b748eeb730
Jordan Hudgens <jordan@wow.com>
2018-01-23 17:04:08 -0600

    Added call to the applyMiddleware module for redux

--- templates/react-redux/src/bootstrap.js
+++ templates/react-redux/src/bootstrap.js
@@ -6,6 +6,8 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
 
