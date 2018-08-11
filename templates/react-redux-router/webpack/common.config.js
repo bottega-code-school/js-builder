522c3c3b5226d3364e737242bd26d0752b55b5c3
jordanhudgens <jordan@devcamp.com>
2018-08-11 12:43:32 -0600

    Updated all of the templates to use the webpack file-loader instead of the deprecated file

--- templates/react-redux-router/webpack/common.config.js
+++ templates/react-redux-router/webpack/common.config.js
@@ -2,56 +2,50 @@
 const SplitChunksPlugin = require('webpack/lib/optimize/SplitChunksPlugin');
 
 module.exports = {
-
   entry: {
-    app: [
-      './src/bootstrap.js'
-    ],
-    vendor: './src/vendor.js'
+    app: ['./src/bootstrap.js'],
+    vendor: './src/vendor.js',
   },
 
   resolve: {
-
     extensions: ['.js', '.scss'],
 
-    modules: ['node_modules']
-
+    modules: ['node_modules'],
   },
 
   module: {
-
     rules: [
-
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ['babel-loader'],
       },
 
       {
         test: /\.json$/,
-        loader: 'json'
+        loader: 'json',
       },
 
       {
         test: /\.(jpg|png|gif|eot|svg|ttf|woff|woff2)$/,
-        loader: 'file',
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+          publicPath: '/',
+        },
       },
 
       {
         test: /\.(mp4|webm)$/,
-        loader: 'url?limit=10000'
-      }
-
-    ]
-
+        loader: 'url?limit=10000',
+      },
+    ],
   },
 
   plugins: [
     new SplitChunksPlugin({
       name: ['app', 'vendor'],
-      minChunks: Infinity
-    })
-  ]
-
+      minChunks: Infinity,
+    }),
+  ],
 };
