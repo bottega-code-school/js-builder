c931039bf7b58d2c0dea07171d246a8d1060aef3
jordanhudgens <jordan@devcamp.com>
2019-03-07 22:41:35 -0600

    Removed jquery and bootstrap from react-redux-router template because... well

--- templates/react-redux-router/src/bootstrap.js
+++ templates/react-redux-router/src/bootstrap.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/app';
-import reducers from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/app";
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-import 'bootstrap/dist/css/bootstrap.css';
-import './style/main.scss';
-
+import "./style/main.scss";
 
 function main() {
   ReactDOM.render(
@@ -18,8 +16,9 @@ function main() {
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
-    , document.querySelector('.app-wrapper'));
+    </Provider>,
+    document.querySelector(".app-wrapper")
+  );
 }
 
-document.addEventListener('DOMContentLoaded', main);
+document.addEventListener("DOMContentLoaded", main);
