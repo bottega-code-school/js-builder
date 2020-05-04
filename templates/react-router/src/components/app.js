402d6c617cc0d634d7a99651553a74e886242ae8
jordanhudgens <jordan@devcamp.com>
2020-05-04 09:08:26 -0700

    Updated naming for the react router template

--- templates/react-router/src/components/app.js
+++ templates/react-router/src/components/app.js
@@ -1,15 +1,11 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 
 export default class App extends Component {
   render() {
     return (
       <div className="app">
         <h1>DevCamp React Starter</h1>
-        <h2>React + Redux</h2>
-
-        <div style={{ color: "red" }}>
-          Warning: this is the experimental template, pardon the bugs
-        </div>
+        <h2>React + React Router</h2>
       </div>
     );
   }
